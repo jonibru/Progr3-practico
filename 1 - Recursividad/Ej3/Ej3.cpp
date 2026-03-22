e74ad51430b498f91107b04d68220a70f827599e
Jonathan Brua <jonathanbrua00@gmail.com>
2026-03-22 20:37:07 -0300

    ej3

--- 1 - Recursividad/Ej3/Ej3.cpp
+++ 1 - Recursividad/Ej3/Ej3.cpp
@@ -4,25 +4,25 @@ y despues el numero invertido*/
 
 #include <iostream>
 
-using namespace std; ////////////////no funciona
+using namespace std;
 
-int invertir(int x, int resto)
+int invertir(int x, int acumulador)
 {
-    if (x == 0)
-    {
-        return x;
-    }
-    else
-        x *= 10;
-    x += resto;
-    return (invertir(x, x % 10));
+	if (x == 0)
+	{
+		return acumulador;
+	}
+	else
+	{
+		return (invertir(x / 10, acumulador * 10 + x % 10));
+	}
 };
 
 int main()
 {
-    int x, resto = 0;
-    cout << "Ingrese numero a invertir: ";
-    cin >> x;
-    cout << "Resultado: " << invertir(x, resto);
-    return 0;
+	int x, resto = 0;
+	cout << "Ingrese numero a invertir: ";
+	cin >> x;
+	cout << "Resultado: " << invertir(x, resto);
+	return 0;
 };
