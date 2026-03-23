14ce2fd737f1e72bdc8891a59c7962211c48ddb9
Jonathan Brua <jonathanbrua00@gmail.com>
2026-03-22 23:17:19 -0300

    ej6

--- 1 - Recursividad/Ej6/Ej6.cpp
+++ 1 - Recursividad/Ej6/Ej6.cpp
@@ -6,13 +6,32 @@ Ej.: si el número es 22005 el resultado es 2, y si fuera 1414 el resultado es 0
 
 using namespace std;
 
-int cantidad_digitos(int n, int i)
+int cantidad_digitos(int n)
 {
     if (n == 0)
     {
         return 0;
     }
-    return cantidad_digitos(n / 10, i + 1);
+    else
+        return 1 + cantidad_digitos(n / 10);
+}
+
+int cantidad_pares(int n, int total, int i)
+{
+    int posicion = total - i;
+    if (n < 10) // caso base
+    {
+        if ((posicion % 2) == 1 && ((n % 10) % 2) == 0)
+            return 1;
+        else
+            return 0;
+    }
+    if ((posicion % 2) == 1 && ((n % 10) % 2) == 0) // sumo 1 si este digito es par en posicion impar + el resto de posiciones
+    {
+        return 1 + cantidad_pares(n / 10, total, i - 1);
+    }
+    else
+        return 0 + cantidad_pares(n / 10, total, i - 1); // sumo el caso de todo el resto de posiciones
 }
 
 int main()
@@ -24,6 +43,8 @@ int main()
         throw "Error. Ingrese entero positivo";
     else
     {
+        int cantidad = cantidad_digitos(n);
+        cout << "Resultado -> " << cantidad_pares(n, cantidad, cantidad) << endl;
     }
 
     return 0;
