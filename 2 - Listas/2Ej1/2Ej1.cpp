56f6a701af7def0cf87d1e07b0d8a10be4efb48c
Jonathan Brua <jonathanbrua00@gmail.com>
2026-03-26 12:24:32 -0300

    carpeta listas corregida

---
+++ 2 - Listas/2Ej1/2Ej1.cpp
@@ -0,0 +1,14 @@
+/*Implementar una función llamada “printLista” que reciba una lista y una posición “p” e
+imprima los elementos de esa lista a partir de la posición “p”. No debe modificarse la lista
+original. Ej: listaOriginal: 1->2->3->4->5->6->7->8->9->10 con p=5 Por pantalla se verá:
+7->8->9->10*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+};
